07ae72dc0b7880c330a5d3faf63824c342cb0f9e
Vince Lee <vslee888@gmail.com>
2016-12-02 23:41:42 +0000

    one move

--- spec/1_BunnyHQSpec.js
+++ spec/1_BunnyHQSpec.js
@@ -1,5 +1,5 @@
 /*
-  global describe, beforeEach, afterEach, it, expect
+  global describe, it, expect
 */
 
 var sut = require('../src/1_BunnyHQ')
@@ -7,5 +7,18 @@ var sut = require('../src/1_BunnyHQ')
 describe('No movement', () => {
   it('should return 0', () => {
     expect(sut.getDistanceAway([])).toEqual(0)
+  })
+})
+
+describe('Single move', () => {
+  describe('- to the left', () => {
+    it('should return same distance', () => {
+      expect(sut.getDistanceAway(['L3'])).toEqual(3)
+    })
+  })
+  describe('- to the right', () => {
+    it('should return same distance', () => {
+      expect(sut.getDistanceAway(['R4'])).toEqual(4)
+    })
   })
 })
--- src/1_BunnyHQ.js
+++ src/1_BunnyHQ.js
@@ -1,8 +1,13 @@
 function TestModel () {
   var self = this
 
-  self.getDistanceAway = () => {
+  self.getDistanceAway = (moves) => {
     let result = 0
+
+    moves.forEach((m) => {
+      const distance = parseInt(m.substring(1, m.length))
+      result += distance
+    })
 
     return result
   }
