e70f63d100429ec96c722a8e63ce8b5368de510a
Vince Lee <vslee888@gmail.com>
2016-12-03 00:04:34 +0000

    massive jump...

--- spec/1_BunnyHQSpec.js
+++ spec/1_BunnyHQSpec.js
@@ -1,24 +1,45 @@
 /*
-  global describe, it, expect
+  global describe, beforeEach, it, expect
 */
 
-var sut = require('../src/1_BunnyHQ')
 
-describe('No movement', () => {
-  it('should return 0', () => {
-    expect(sut.getDistanceAway([])).toEqual(0)
+describe('BunnyHQ map', () => {
+  let sut = null
+
+  beforeEach(() => {
+    sut = require('../src/1_BunnyHQ')
   })
-})
 
-describe('Single move', () => {
-  describe('- to the left', () => {
-    it('should return same distance', () => {
-      expect(sut.getDistanceAway(['L3'])).toEqual(3)
+  describe('No movement', () => {
+    it('should return 0', () => {
+      expect(sut.getDistanceAway([])).toEqual(0)
     })
   })
-  describe('- to the right', () => {
-    it('should return same distance', () => {
-      expect(sut.getDistanceAway(['R4'])).toEqual(4)
+
+  describe('Single move', () => {
+    describe('- to the left', () => {
+      it('should return same distance', () => {
+        expect(sut.getDistanceAway(['L3'])).toEqual(3)
+      })
+    })
+    describe('- to the right', () => {
+      it('should return same distance', () => {
+        expect(sut.getDistanceAway(['R4'])).toEqual(4)
+      })
     })
   })
+
+  describe('Three moves', () => {
+    describe('- clockwise in a square', () => {
+      it('should return initial distance', () => {
+        expect(sut.getDistanceAway(['R2', 'R2', 'R2'])).toEqual(2)
+      })
+    })
+    describe('- anti-clockwise in a square', () => {
+      it('should return initial distance', () => {
+        expect(sut.getDistanceAway(['L2', 'L2', 'L2'])).toEqual(2)
+      })
+    })
+  })
+
 })
--- src/1_BunnyHQ.js
+++ src/1_BunnyHQ.js
@@ -1,15 +1,46 @@
+const orientations = {
+  north: 0,
+  east: 1,
+  south: 2,
+  west: 3
+}
+
+function Santa () {
+  const self = this
+  self.orientation = orientations.north
+
+  self.getNewOrientation = (direction) => {
+    const modifier = direction === 'R' ? 1 : -1
+    self.orientation += modifier
+    if (self.orientation > orientations.west) return orientations.north
+    if (self.orientation < orientations.north) return orientations.west
+    return self.orientation
+  }
+
+  self.turn = (direction) => {
+    self.orientation = self.getNewOrientation(direction)
+  }
+}
+
 function TestModel () {
-  var self = this
+  const self = this
+  const santa = new Santa()
 
   self.getDistanceAway = (moves) => {
-    let result = 0
+    let stepsNorth = 0
+    let stepsEast = 0
 
     moves.forEach((m) => {
+      const direction = m.substring(0, 1)
+      santa.turn(direction)
       const distance = parseInt(m.substring(1, m.length))
-      result += distance
+      if (santa.orientation === orientations.north) { stepsNorth += distance }
+      if (santa.orientation === orientations.east) { stepsEast += distance }
+      if (santa.orientation === orientations.south) { stepsNorth -= distance }
+      if (santa.orientation === orientations.west) { stepsEast -= distance }
     })
 
-    return result
+    return Math.abs(stepsNorth) + Math.abs(stepsEast)
   }
 }
 
