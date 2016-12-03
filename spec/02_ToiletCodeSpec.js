f805b6a68fb5fe56d5aeaf2e2142326b8401b5a4
Vince Lee <vslee888@gmail.com>
2016-12-03 01:26:41 +0000

    tidy up interface

--- spec/02_ToiletCodeSpec.js
+++ spec/02_ToiletCodeSpec.js
@@ -4,7 +4,18 @@
 import getToiletCode from '../src/02_ToiletCode/getToiletCode'
 
 describe('Get Toilet Code', () => {
+  const keypad = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ]
+  const startPosition = {
+    buttonsAcross: 2,
+    buttonsDown: 2
+  }
+
   it('- Should start at 5', () => {
-    expect(getToiletCode('')).toEqual('5')
+    const moves = ''
+    expect(getToiletCode(keypad, startPosition, moves)).toEqual('5')
   })
 })
--- src/02_ToiletCode/getToiletCode.js
+++ src/02_ToiletCode/getToiletCode.js
@@ -1,3 +1,15 @@
-module.exports = (moves) => {
-  return '5'
+class Keypad {
+  constructor (layout) {
+    this.layout = layout
+  }
+
+  getValue ({ buttonsAcross, buttonsDown }) {
+    return this.layout[buttonsDown - 1][buttonsAcross - 1]
+  }
+}
+
+module.exports = (keypadLayout, startPosition, moves) => {
+  const keypad = new Keypad(keypadLayout)
+  const currValue = keypad.getValue(startPosition)
+  return currValue.toString()
 }
