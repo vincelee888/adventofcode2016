b70993c5445e4429efca4265e016a52820a82123
Vince Lee <vslee888@gmail.com>
2016-12-05 19:27:50 +0000

    valid triangle is a + b > c

--- spec/03_ValidTrianglesSpec.js
+++ spec/03_ValidTrianglesSpec.js
@@ -7,4 +7,8 @@ describe('Get Total Valid Triangles', () => {
   it('- returns an integer', () => {
     expect(sut()).toEqual(0)
   })
+
+  it('- true if sum of two smallest values is greater than the other number', () => {
+    expect(sut('1 2 2')).toEqual(1)
+  })
 })
--- src/03_ValidateTriangles/getTotalValidTriangles.js
+++ src/03_ValidateTriangles/getTotalValidTriangles.js
@@ -1,36 +1,10 @@
-class Keypad {
-  constructor (layout) {
-    this.layout = layout
+module.exports = (values) => {
+  let count = 0
+  if (values !== undefined && values.length) {
+    let [a, b, c] = values
+      .split(' ')
+      .map((x) => parseInt(x))
+    if (a + b > c) { count++ }
   }
-
-  getValue ({ buttonsAcross, buttonsDown }) {
-    return this.layout[buttonsDown - 1][buttonsAcross - 1]
-  }
-}
-
-class Plotter {
-  constructor (layout) {
-    this.layout = layout
-  }
-
-  getDigit ({ buttonsAcross, buttonsDown }, moves) {
-    moves
-      .split('')
-      .forEach((m) => {
-        if (m === 'U') { buttonsDown-- }
-        if (m === 'R') { buttonsAcross++ }
-        if (m === 'D') { buttonsDown++ }
-        if (m === 'L') { buttonsAcross-- }
-
-        if (buttonsDown <= 0) buttonsDown = 1
-        if (buttonsAcross > this.layout[0].length) buttonsAcross = this.layout[0].length
-        if (buttonsDown > this.layout.length) buttonsDown = this.layout.length
-        if (buttonsAcross <= 0) buttonsAcross = 1
-      })
-    return { buttonsAcross, buttonsDown }
-  }
-}
-
-module.exports = () => {
-  return 0
+  return count
 }
