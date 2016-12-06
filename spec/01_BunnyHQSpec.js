13a1e1eb3b6876f955ec79265b66d376720fc652
Vince Lee <vslee888@gmail.com>
2016-12-06 10:42:36 +0000

    part 2 solution

--- spec/01_BunnyHQSpec.js
+++ spec/01_BunnyHQSpec.js
@@ -53,4 +53,10 @@ describe('BunnyHQ map', () => {
       expect(getFirstDoubleVisited('R8, R4, R4, R8')).toEqual(4)
     })
   })
+
+  describe('- real solution (part 2)', () => {
+    it('- should return blocks away', () => {
+      expect(getFirstDoubleVisited('L3, R1, L4, L1, L2, R4, L3, L3, R2, R3, L5, R1, R3, L4, L1, L2, R2, R1, L4, L4, R2, L5, R3, R2, R1, L1, L2, R2, R2, L1, L1, R2, R1, L3, L5, R4, L3, R3, R3, L5, L190, L4, R4, R51, L4, R5, R5, R2, L1, L3, R1, R4, L3, R1, R3, L5, L4, R2, R5, R2, L1, L5, L1, L1, R78, L3, R2, L3, R5, L2, R2, R4, L1, L4, R1, R185, R3, L4, L1, L1, L3, R4, L4, L1, R5, L5, L1, R5, L1, R2, L5, L2, R4, R3, L2, R3, R1, L3, L5, L4, R3, L2, L4, L5, L4, R1, L1, R5, L2, R4, R2, R3, L1, L1, L4, L3, R4, L3, L5, R2, L5, L1, L1, R2, R3, L5, L3, L2, L1, L4, R4, R4, L2, R3, R1, L2, R1, L2, L2, R3, R3, L1, R4, L5, L3, R4, R4, R1, L2, L5, L3, R1, R4, L2, R5, R4, R2, L5, L3, R4, R1, L1, R5, L3, R1, R5, L2, R1, L5, L2, R2, L2, L3, R3, R3, R1')).toEqual(143)
+    })
+  })
 })
