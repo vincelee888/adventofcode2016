e88ab54961672b9120a6e44a02cb986fc52bb90a
Vince Lee <vslee888@gmail.com>
2016-12-06 12:22:27 +0000

    part can have multiple occurrences

--- spec/04_SecurityThroughObscuritySpec.js
+++ spec/04_SecurityThroughObscuritySpec.js
@@ -12,7 +12,11 @@ describe('Get sum of valid site ids', () => {
     expect(sumValidSiteIds('-123[]')).toEqual(123)
   })
 
-  it('- part must appear in checksum', () => {
+  it('- part id must appear in checksum', () => {
     expect(sumValidSiteIds('b-123[a]')).toEqual(0)
+  })
+
+  it('- part id can have multiple occurrences', () => {
+    expect(sumValidSiteIds('aa-123[a]')).toEqual(123)
   })
 })
--- src/04_SecurityThroughObscurity/app.js
+++ src/04_SecurityThroughObscurity/app.js
@@ -1,20 +1,21 @@
 const isValid = ({parts, checksum}) => {
-  return parts[0] === checksum
+  return parts[0].charAt(0) === checksum
 }
 
 const parse = (input) => {
   const split = input.split('[')
   const checksum = split[1].substring(0, split[1].length - 1)
   const parts = split[0].split('-')
-  const siteId = parts.pop()
+  const siteId = parseInt(parts.pop())
   return { parts, siteId, checksum }
 }
 
 const sumValidSiteIds = (input) => {
   if (input === undefined) return 0
+
   const elements = parse(input)
   if (isValid(elements)) {
-    return parseInt(elements.siteId)
+    return elements.siteId
   }
 
   return 0
