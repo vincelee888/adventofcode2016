1962fea282112475f10c88e80bf5ba46aa0d494a
Vince Lee <vslee888@gmail.com>
2016-12-06 09:12:28 +0000

    use reduce

--- src/03_ValidateTriangles/getTotalValidTriangles.js
+++ src/03_ValidateTriangles/getTotalValidTriangles.js
@@ -1,16 +1,26 @@
+const isValidTriangle = ([a, b, c]) => {
+  return a + b > c
+}
+
+const parseLine = (line) => {
+  return line
+    .trim()
+    .split(/\s+/)
+    .map((x) => parseInt(x))
+}
+
+const parse = (values) => {
+  return values
+    .split('\n')
+      .reduce((acc, cur) => {
+        const [a, b, c] = parseLine(cur)
+        return isValidTriangle([a, b, c])
+          ? acc + 1
+          : acc
+      }, 0)
+}
+
 module.exports = (values) => {
-  let count = 0
-  if (values !== undefined && values.length) {
-    values
-      .split('\n')
-      .forEach((v) => {
-        console.log(v)
-        let [a, b, c] = v
-          .trim()
-          .split(/\s+/)
-          .map((x) => parseInt(x))
-        if (a + b > c) { count++ }
-      })
-  }
-  return count
+  if (values === undefined) return 0
+  return parse(values)
 }
