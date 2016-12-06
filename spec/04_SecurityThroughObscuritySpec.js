921490d8b7b7f21f981e90a2e502bbc0a0839228
Vince Lee <vslee888@gmail.com>
2016-12-06 15:25:58 +0000

    reduce

--- spec/04_SecurityThroughObscuritySpec.js
+++ spec/04_SecurityThroughObscuritySpec.js
@@ -1,30 +1,47 @@
 /*
   global describe,  it, expect
 */
-import { sumValidSiteIds } from '../src/04_SecurityThroughObscurity/app'
+import { sumValidSectorIds } from '../src/04_SecurityThroughObscurity/app'
+import { part1 } from './inputs/04'
 
 describe('Get sum of valid site ids', () => {
   it('- Should return an integer', () => {
-    expect(sumValidSiteIds()).toEqual(0)
+    expect(sumValidSectorIds()).toEqual(0)
   })
 
-  it('- encoding is {parts}-{siteId}[{checksum}]', () => {
-    expect(sumValidSiteIds('-123[]')).toEqual(123)
+  it('- encoding is {characters}-{sectorId}[{checksum}]', () => {
+    expect(sumValidSectorIds('-123[]')).toEqual(123)
   })
 
-  it('- part id must appear in checksum', () => {
-    expect(sumValidSiteIds('b-123[a]')).toEqual(0)
+  it('- character must appear in checksum', () => {
+    expect(sumValidSectorIds('b-123[a]')).toEqual(0)
   })
 
-  it('- part id can have multiple occurrences', () => {
-    expect(sumValidSiteIds('aa-123[a]')).toEqual(123)
+  it('- character can have multiple occurrences', () => {
+    expect(sumValidSectorIds('aa-123[a]')).toEqual(123)
   })
 
-  it('- part id count denotes position in checksum', () => {
-    expect(sumValidSiteIds('b-aa-123[ab]')).toEqual(123)
+  it('- character count denotes position in checksum', () => {
+    expect(sumValidSectorIds('b-aa-123[ab]')).toEqual(123)
   })
 
-  it('- part with same priority are ordered alphabetically', () => {
-    expect(sumValidSiteIds('c-b-aa-123[abc]')).toEqual(123)
+  it('- characters with same count are ordered alphabetically', () => {
+    expect(sumValidSectorIds('c-b-aa-123[abc]')).toEqual(123)
+  })
+
+  it('- characters with same count are ordered alphabetically', () => {
+    expect(sumValidSectorIds('c-b-aa-123[abc]')).toEqual(123)
+  })
+
+  it('- checksum is first five characters', () => {
+    expect(sumValidSectorIds('c-b-aa-d-e-fff-123[fabcd]')).toEqual(123)
+  })
+
+  it('- can handle multiple sites', () => {
+    const input = `not-a-real-room-404[oarel]
+    a-b-c-d-e-f-g-h-987[abcde]
+    aaaaa-bbb-z-y-x-123[abxyz]
+    totally-real-room-200[decoy]`
+    expect(sumValidSectorIds(input)).toEqual(1514)
   })
 })
--- src/04_SecurityThroughObscurity/app.js
+++ src/04_SecurityThroughObscurity/app.js
@@ -1,12 +1,27 @@
-const isValid = ({parts, checksum}) => {
-  const sorted = parts
-    .map((p) => ({ char: p.charAt(0), priority: p.length }))
-    .sort((a, b) => {
-      if (b.priority < a.priority) return -1
-      if (b.priority > a.priority) return 1
-
-      return b.char < a.char
+const getCounts = (letters) => {
+  const counts = []
+  letters
+    .forEach((l) => {
+      const match = counts.filter((c) => c.char === l)
+      if (match.length === 1) {
+        match[0].priority++
+      } else {
+        counts.push({ char: l, priority: 1 })
+      }
     })
+  return counts
+}
+
+const sortByCountThenAlpha = (a, b) => {
+  if (b.priority < a.priority) return -1
+  if (b.priority > a.priority) return 1
+
+  return b.char < a.char
+}
+
+const isValid = ({ letters, checksum }) => {
+  const sorted = getCounts(letters)
+    .sort(sortByCountThenAlpha)
 
   return checksum.split('')
     .map((el, i) => el !== sorted[i].char)
@@ -17,19 +32,20 @@ const parse = (input) => {
   const split = input.split('[')
   const checksum = split[1].substring(0, split[1].length - 1)
   const parts = split[0].split('-')
-  const siteId = parseInt(parts.pop())
-  return { parts, siteId, checksum }
+  const sectorId = parseInt(parts.pop())
+  const letters = parts.join('').split('')
+  return { letters, sectorId, checksum }
 }
 
-const sumValidSiteIds = (input) => {
+const sumValidSectorIds = (input) => {
   if (input === undefined) return 0
-
-  const elements = parse(input)
-  if (isValid(elements)) {
-    return elements.siteId
-  }
-
-  return 0
+  return input
+    .split('\n')
+    .map((l) => parse(l.trim()))
+    .filter((e) => isValid(e))
+    .reduce((a, b) => {
+      return a + b.sectorId
+    }, 0)
 }
 
-module.exports = { sumValidSiteIds }
+module.exports = { sumValidSectorIds }
