4549fe42c89326b901f93697a3f9c003c823c4c2
Vince Lee <vslee888@gmail.com>
2016-12-16 22:36:20 +0000

    day 8

--- spec/08_PixelsSpec.js
+++ spec/08_PixelsSpec.js
@@ -1,59 +1,145 @@
 /*
   global describe,  it, expect
 */
-import { newScreen, rect, rotateRow } from '../src/08_Pixels/app'
+import * as pixel from '../src/08_Pixels/app'
+import { testData } from './inputs/08'
 
 describe('Screen', () => {
   it('- should return default to 1x1', () => {
-    expect(newScreen()).toEqual('-')
+    expect(pixel.newScreen()).toEqual('-')
   })
   it('- can specify width', () => {
-    expect(newScreen(3)).toEqual('---')
+    expect(pixel.newScreen(3)).toEqual('---')
   })
   it('- can specify height', () => {
     const expected = `-
 -
 -`
-    expect(newScreen(1, 3)).toEqual(expected)
+    expect(pixel.newScreen(1, 3)).toEqual(expected)
   })
 })
 
 describe('Rect', () => {
   it('- should turn on cells in the top left', () => {
-    const start = newScreen()
-    expect(rect(start)).toEqual('*')
+    const start = pixel.newScreen()
+    expect(pixel.rect(start)).toEqual('*')
   })
   it('- can specify width', () => {
-    const start = newScreen(3)
-    expect(rect(start, 2)).toEqual('**-')
+    const start = pixel.newScreen(3)
+    expect(pixel.rect(start, 2)).toEqual('**-')
   })
   it('- can specify height', () => {
-    const start = newScreen(3, 3)
+    const start = pixel.newScreen(3, 3)
     const expected = `**-
 **-
 ---`
-    expect(rect(start, 2, 2)).toEqual(expected)
+    expect(pixel.rect(start, 2, 2)).toEqual(expected)
   })
 })
 
 describe('Rotate Row', () => {
-  it('- should shift the pixel state by the requested cells', () => {
+  it('- should do nothing by default', () => {
     const start = '*----'
-    expect(rotateRow(start)).toEqual('*----')
+    expect(pixel.rotateRow(start)).toEqual('*----')
   })
   it('- should shift the pixel state by the requested cells', () => {
     const start = '*----'
-    expect(rotateRow(start, 2)).toEqual('--*--')
+    expect(pixel.rotateRow(start, 2)).toEqual('--*--')
   })
   it('- should wrap cells', () => {
     const start = '----*'
-    expect(rotateRow(start, 1)).toEqual('*----')
+    expect(pixel.rotateRow(start, 1)).toEqual('*----')
   })
   it('- can specify which row to shift', () => {
     const start = `*----
 *----`
     const expected = `*----
 -*---`
-    expect(rotateRow(start, 1, 1)).toEqual(expected)
+    expect(pixel.rotateRow(start, 1, 1)).toEqual(expected)
+  })
+})
+
+describe('Rotate Column', () => {
+  it('- should do nothing by default', () => {
+    const start = `*
+-
+-`
+    expect(pixel.rotateColumn(start)).toEqual(start)
+  })
+  it('- should shift the pixel state by the requested cells', () => {
+    const start = `*
+-
+-`
+    const expected = `-
+*
+-`
+    expect(pixel.rotateColumn(start, 1)).toEqual(expected)
+  })
+  it('- should shift the pixel state of the requested column', () => {
+    const start = `-*-
+---
+---`
+    const expected = `---
+-*-
+---`
+    expect(pixel.rotateColumn(start, 1, 1)).toEqual(expected)
+  })
+  it('- should wrap indefinitely', () => {
+    const start = `-*-
+---
+---`
+    const expected = `---
+-*-
+---`
+    expect(pixel.rotateColumn(start, 10, 1)).toEqual(expected)
+  })
+})
+
+describe('Text commands', () => {
+  it('- rect axb turns on rectangle', () => {
+    const start = pixel.newScreen(3, 3)
+    const expected = `***
+***
+---`
+    expect(pixel.transform('rect 3x2', start)).toEqual(expected)
+  })
+
+  it('- rotate column x=a by b rotates column a by b pixels', () => {
+    const start = `-*-
+---
+---`
+    const expected = `---
+-*-
+---`
+    expect(pixel.transform('rotate column x=1 by 1', start)).toEqual(expected)
+  })
+
+  it('- rotate row y=a by b rotates row a by b pixels', () => {
+    const start = `*----
+*----`
+    const expected = `-*---
+*----`
+    expect(pixel.transform('rotate row y=0 by 1', start)).toEqual(expected)
+  })
+
+  it('- can execute multiple commands', () => {
+    const start = pixel.newScreen(3, 3)
+    const commands = `rect 2x2
+rotate row y=1 by 1`
+    const expected = `**-
+-**
+---`
+    expect(pixel.processInput(start, commands)).toEqual(expected)
+  })
+
+  it('- can solve part 1', () => {
+    const start = pixel.newScreen(50, 6)
+    const processed = pixel.processInput(start, testData)
+    const onCount = processed
+      .replace('\n', '')
+      .split('')
+      .filter((c) => c === '*')
+      .length
+    expect(onCount).toEqual(121)
   })
 })
--- src/08_Pixels/app.js
+++ src/08_Pixels/app.js
@@ -1,11 +1,12 @@
 const off = '-'
 const on = '*'
+const newLine = '\n'
 
 export const newScreen = (width = 1, height = 1) => {
   let line = off.repeat(width)
   let result = ''
   for (let i = 0; i < height; i++) {
-    if (i > 0) { result += '\n' }
+    if (i > 0) { result += newLine }
     result += line
   }
   return result
@@ -16,13 +17,13 @@ const switchOnLine = (line, width = 1) => {
 }
 
 export const rect = (screen, width = 1, height = 1) => {
-  const rows = screen.split('\n')
+  const rows = screen.split(newLine)
   for (let rowIndex in rows) {
     if (rowIndex < height) {
       rows[rowIndex] = switchOnLine(rows[rowIndex], width)
     }
   }
-  return rows.join('\n')
+  return rows.join(newLine)
 }
 
 const rotate = (line, cellsToMove) => {
@@ -31,7 +32,81 @@ const rotate = (line, cellsToMove) => {
 }
 
 export const rotateRow = (screen, cellsToMove, rowToRotate = 0) => {
-  const rows = screen.split('\n')
+  const rows = screen.split(newLine)
   rows[rowToRotate] = rotate(rows[rowToRotate], cellsToMove)
-  return rows.join('\n')
+  return rows.join(newLine)
+}
+
+const reInsertColumn = (screen, column, columnIndex) => {
+  const screenRows = screen.split(newLine)
+  const columnChars = column.split('')
+  for (let rowIndex in screenRows) {
+    const row = screenRows[rowIndex]
+    const left = row.substr(0, columnIndex)
+    const newChar = columnChars[rowIndex]
+    const right = row.substr(columnIndex + 1, row.length - columnIndex)
+    screenRows[rowIndex] = `${left}${newChar}${right}`
+  }
+  return screenRows.join(newLine)
+}
+
+export const rotateColumn = (screen, cellsToMove = 0, columnIndex = 0) => {
+  const rows = screen
+    .split(newLine)
+  cellsToMove = cellsToMove % rows.length
+  const columnState = rows
+    .reduce((a, b) => a + b.charAt(columnIndex), '')
+  const updated = rotate(columnState, cellsToMove)
+  return reInsertColumn(screen, updated, columnIndex)
+}
+
+const rectFunc = {
+  'identifier': (c) => c.startsWith('rect'),
+  'getParams': (c) => {
+    return c.split(' ')[1].split('x')
+  },
+  'exec': (screen, [width, height]) => {
+    return rect(screen, width, height)
+  }
+}
+
+const rotateColumnFunc = {
+  'identifier': (c) => c.startsWith('rotate column x'),
+  'getParams': (c) => {
+    return c.split('=')[1].split(' by ')
+  },
+  'exec': (screen, [columnIndex, cellsToMove]) => {
+    return rotateColumn(screen, cellsToMove, parseInt(columnIndex))
+  }
+}
+
+const rotateRowFunc = {
+  'identifier': (c) => c.startsWith('rotate row y'),
+  'getParams': (c) => {
+    return c.split('=')[1].split(' by ')
+  },
+  'exec': (screen, [rowIndex, cellsToMove]) => {
+    return rotateRow(screen, cellsToMove, parseInt(rowIndex))
+  }
+}
+
+const funcs = [rectFunc, rotateColumnFunc, rotateRowFunc]
+
+const getFuncType = (command) => {
+  return funcs
+    .filter((f) => f.identifier(command))[0]
+}
+
+export const transform = (commandText, screen) => {
+  const funcType = getFuncType(commandText)
+  const params = funcType.getParams(commandText)
+  return funcType.exec(screen, params)
+}
+
+export const processInput = (screen, commands) => {
+  const output = commands
+    .split(newLine)
+    .reduce((a, b) => transform(b, a), screen)
+  console.log(output)
+  return output
 }
